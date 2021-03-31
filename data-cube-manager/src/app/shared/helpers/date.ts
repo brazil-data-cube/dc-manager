db403a254ddb897f292697b39ad38f0077e16b9a
raphaelrpl <raphael.wcosta@gmail.com>
2021-03-31 13:39:50 -0300

    Set start_date and end_date as utc (close #66)

--- data-cube-manager/src/app/shared/helpers/date.ts
+++ data-cube-manager/src/app/shared/helpers/date.ts
@@ -2,10 +2,10 @@
  * convert date to string with USA template
  */
 export function formatDateUSA(date: Date): string {
-    let month: string = (date.getMonth() + 1).toString();
+    let month: string = (date.getUTCMonth() + 1).toString();
     month = month.toString().length === 1 ? `0${month}` : month;
-    let day: string = (date.getDate()).toString();
+    let day: string = (date.getUTCDate()).toString();
     day = day.toString().length === 1 ? `0${day}` : day;
 
-    return `${date.getFullYear()}-${month}-${day}`;
+    return `${date.getUTCFullYear()}-${month}-${day}`;
 }
