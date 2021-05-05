7566d669a38ac1f8ad5527e4a365ca87508f37af
raphaelrpl <raphael.wcosta@gmail.com>
2021-05-05 15:27:36 -0300

    Fix select grid tiles using draw polygon (close #74)

--- data-cube-manager/src/app/admin/pages/create-cube/steps/images/images.component.ts
+++ data-cube-manager/src/app/admin/pages/create-cube/steps/images/images.component.ts
@@ -14,6 +14,7 @@ import { formatDateUSA } from 'app/shared/helpers/date';
 import { setBandsAvailable, setRangeTemporal, setTiles, setStacList, setSatellite } from 'app/admin/admin.action';
 
 import { intersect } from '@turf/turf';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-create-cube-images',
@@ -58,6 +59,8 @@ export class CreateCubeImagesComponent implements OnInit {
       startDate: ['', [Validators.required]],
       lastDate: ['', [Validators.required]]
     });
+    this.tiles = [];
+    this.featuresSelected = [];
   }
 
   ngOnInit() {
@@ -227,7 +230,7 @@ export class CreateCubeImagesComponent implements OnInit {
           verticalPosition: 'top',
           panelClass: 'app_snack-bar-error'
         });
-          
+
       } else {
         try {
           this.store.dispatch(showLoading());
@@ -283,7 +286,7 @@ export class CreateCubeImagesComponent implements OnInit {
           }
 
           this.store.dispatch(setBandsAvailable({ bands }));
-          this.store.dispatch(setStacList({ 
+          this.store.dispatch(setStacList({
             stacList: this.stacList.filter(s => s.url && s.collection)
           }));
 
@@ -337,7 +340,7 @@ export class CreateCubeImagesComponent implements OnInit {
 
   /**
    * set Draw control of the map
-   */ 
+   */
   private setDrawControl() {
     let drawnItems = new FeatureGroup();
     this.map.addLayer(drawnItems);
@@ -358,34 +361,16 @@ export class CreateCubeImagesComponent implements OnInit {
       },
       edit: {
         featureGroup: drawnItems,
-        remove: null
+        remove: null,
+        edit: false
       }
     });
     this.map.addControl(drawControl);
-
-    // remove style of grid tiles
-    this.map.on(Draw.Event.DRAWSTART, e => {
-      if (e['layerType'] == 'polygon') {
-        return;
-      }
-
-      this.map.eachLayer(l => {
-        if (l.getAttribution() && l.getAttribution().indexOf('BDC-') >= 0) {
-          const layer: any = l
-          layer.setStyle({
-            fillOpacity: 0.1,
-            fillColor:'blue'
-          })
-        }
-      })
-    })
 
     // add bbox in the map
     this.map.on(Draw.Event.CREATED, e => {
       const layer = e.layer;
       const newLayer = layer.toGeoJSON();
-      this.tiles = []
-      this.featuresSelected = []
 
       this.map.eachLayer(l => {
         if (l.getAttribution() && l.getAttribution().indexOf('BDC-') >= 0) {
@@ -414,5 +399,64 @@ export class CreateCubeImagesComponent implements OnInit {
   onMapReady(map: MapLeaflet) {
     this.map = map
     this.setDrawControl()
+    this.setClearSelectedFeaturesControl();
+  }
+
+  private setClearSelectedFeaturesControl() {
+    const self = this;
+
+    const controlType = L.Control.extend({
+      options: {
+        position: 'topleft',
+        title: 'Clear Selected Features',
+      },
+      onAdd: function(map) {
+        let container = L.DomUtil.create('div', 'leaflet-bar');
+        let className = 'leaflet-control-clear-features';
+
+        this._createButton(this.options.title, className, container, this._clearFeatures, this);
+
+        return container;
+      },
+      _createButton(title, className, container, fn, context) {
+        this.link = L.DomUtil.create('a', className, container);
+        this.link.href = '#';
+        this.link.title = title;
+
+        const icon = L.DomUtil.create('span', 'material-icons', this.link);
+        icon.append('delete');
+        icon.style.fontSize = '20px';
+        icon.style.color = '#404040';
+
+        this.link.setAttribute('role', 'button');
+        this.link.setAttribute('aria-label', title);
+
+        L.DomEvent
+          .addListener(this.link, 'click', L.DomEvent.stopPropagation)
+          .addListener(this.link, 'click', L.DomEvent.preventDefault)
+          .addListener(this.link, 'click', fn, context);
+
+        return this.link;
+      },
+      _clearFeatures() {
+        const map = this._map;
+
+        for (let key of Object.keys(map._layers)) {
+          const layer: any = map._layers[key];
+
+          if (layer.hasOwnProperty('feature') && self.tiles.includes(layer.feature['geometry']['id'])) {
+            layer.setStyle({
+              fillOpacity: 0.1,
+              fillColor:'blue'
+            })
+            const index = self.tiles.indexOf(layer.feature['geometry']['id']);
+            self.tiles.splice(index, 1);
+          }
+        }
+      }
+    });
+
+    const control = new controlType();
+    control.addTo(this.map);
   }
 }
