3f3b334bf46f3da228adf2fca772506063d9e59d
raphaelrpl <raphael.wcosta@gmail.com>
2021-07-22 09:52:56 -0300

    :sparkles: Add dialog to update data cube bands (close #83)

--- data-cube-manager/src/app/admin/pages/details-cube/details-cube.component.ts
+++ data-cube-manager/src/app/admin/pages/details-cube/details-cube.component.ts
@@ -10,6 +10,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ReprocessDialogComponent } from 'app/admin/components/reprocess-dialog/reprocess-dialog.component';
 import { UpdateCubeDialog } from 'app/admin/components/update-cube-dialog/update-cube-dialog.component';
 import { DomSanitizer } from '@angular/platform-browser';
+import { UpdateBandDialogComponent } from 'app/admin/components/update-band-dialog/update-band-dialog.component';
 
 @Component({
     selector: 'app-details-cube',
@@ -198,6 +199,24 @@ export class DetailsCubeComponent implements OnInit {
             data: {
                 cube: this.cube,
                 parameters
+            }
+        })
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.getCube(this.cube['id']);
+            }
+        });
+    }
+
+    async openUpdateBandModal(band: any) {
+        const dialogRef = this.dialog.open(UpdateBandDialogComponent, {
+            width: '600px',
+            maxHeight: '85%',
+            minHeight: '400px',
+            disableClose: true,
+            data: {
+                cube: this.cube,
+                band
             }
         })
         dialogRef.afterClosed().subscribe(result => {
